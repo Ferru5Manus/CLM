14a9f4924fb47f965f9659baf3797eb43e26cd75
FerrusManus <roma.hlinin@yandex.ru>
2022-02-07 11:59:53 +0300

    AddingAccountsCommit

--- SchoolDatabaseRepository/Accounts/AccountsDto.cs
+++ SchoolDatabaseRepository/Accounts/AccountsDto.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolSite100._0
+namespace SchoolDatabaseRepository
 {
-    public class User
+    public class AccountsDto
     {
-        public int id { get; set; }
-        public string email { get; set; }
+        public int Id { get; set; }
         public string login { get; set; }
+        public string email { get; set; }
         public string password { get; set; }
         public string role { get; set; }
         public string form { get; set; }
-        
     }
 }
---
+++ SchoolDatabaseRepository/Accounts/IAccountsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDatabaseRepository
+{
+    public interface IAccountsRepository
+    {
+        void AddAccount(AccountsDto account);
+        List<string> GetAllUsersIds();
+        List<string> GetUserLogins();
+        List<string> GetUserEmails();
+        List<string> GetUserRoles();
+        List<string> GetUserForms();
+        void UpdateLogin(AccountsDto account);
+        void UpdateEmail(AccountsDto account);
+        void UpdateForm(AccountsDto account);
+        void UpdateRole(AccountsDto account);
+        void GetPassword(AccountsDto accounts);
+        List<string> GetUserByLogin(AccountsDto account);
+    }
+}
